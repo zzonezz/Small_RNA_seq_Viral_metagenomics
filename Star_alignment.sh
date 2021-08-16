afac0c124627bdb0c7818b1f3851296c0aa4ad39
zzonezz <89038504+zzonezz@users.noreply.github.com>
2021-08-16 14:52:43 -0400

    Create Star_alignment.sh
    Run STAR alignment

---
+++ Star_alignment.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo -n "please input your main sqeuencing reulst folder:"
+read inputpath
+echo -n "Please input your destination folder:"
+read outputpath
+echo -n "please input your genomeDir folder for STAR alignment:"
+read genomeDir #/projectnb/lau-bumc/zhengzhu/AaegL5/ncbi_dataset/data/GCA_002204515.1
+echo -n "please input your sjdbGTFfile folder for STAR alignment:"
+read sjdbGTFfile #/projectnb/lau-bumc/zhengzhu/AaegL5/ncbi_dataset/data/GCA_002204515.1/GCF_002204515.2_AaegL5.0_genomic.gtf
+mkdir starresults
+module load star
+    for file in `ls $outputpath`
+        do
+            if [[ "$file" =~ .fasta$ ]]; then
+              STAR --runThreadN 28   --outFileNamePrefix $outputpath/starresults/${file%.*} --outReadsUnmapped Fastx --quantMode GeneCounts --genomeDir $genomeDir --readFilesIn $outputpath/$file --sjdbGTFfile $sjdbGTFfile --outFilterMismatchNoverLmax 0.05 --outFilterMatchNmin 16 --outFilterScoreMinOverLread 0  --outFilterMatchNminOverLread 0 --alignIntronMax 1
+            fi
+          done
+fi
