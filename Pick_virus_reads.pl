5c56e596e939ef77378b1a1ce45ab6dd1cb021d7
zzonezz <89038504+zzonezz@users.noreply.github.com>
2022-02-17 11:52:12 -0500

    Update Pick_virus_reads.pl

--- Pick_virus_reads.pl
+++ Pick_virus_reads.pl
@@ -11,7 +11,7 @@ foreach(<IN>){
   chomp;
     my @a= split("\t",$_);
     #if (/(.*)\t(.*)\t(.*)\t(.*)\t(.*)\t(.*)\t(.*)\t(.*)\t(.*)\t(.*)\t(.*)\t(.*)\t(.*)\t(.*)/){}
-#0:readsname;1accession;2E;3start;4end;5qu_len;6ma_start;7map_end;8iden;9coverage;10kidom;11species;12commname;13blatnam
+#0:readsname;1accession;2E;3start;4end;5qu_len;6ref_start;7ref_end;8iden;9coverage;10kingdom;11species;12commname;13blatnam
 if ($a[10] =~ /Viruses/){$hs{"$a[0]"}=$a[11];}
 };
 close IN;
